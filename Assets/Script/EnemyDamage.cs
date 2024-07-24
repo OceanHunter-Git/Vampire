f234d0466a6c193ec3f03f6820513aed6bb1b47b
wangchengze <13056790885@163.com>
2024-07-24 16:11:55 +0800

    fireball's slowly appear and disappear

--- Assets/Script/EnemyDamage.cs
+++ Assets/Script/EnemyDamage.cs
@@ -6,19 +6,28 @@ public class EnemyDamage : MonoBehaviour
 {
     public float lifetime;
     public float damageAmount;
+    public float growthSpeed;
+    private Vector3 targetSize;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetSize = transform.localScale;
+        transform.localScale = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.localScale = Vector3.MoveTowards(transform.localScale, targetSize, growthSpeed * Time.deltaTime);
         lifetime -= Time.deltaTime;
         if (lifetime <= 0)
         {
-            Destroy(gameObject);
+            targetSize = Vector3.zero;
+            if (transform.localScale == Vector3.zero)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
