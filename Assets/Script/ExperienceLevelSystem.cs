d0f4093636667f4d191cdd6a69c630adae16e911
wangchengze <13056790885@163.com>
2024-07-25 19:43:46 +0800

    Level UP

--- Assets/Script/ExperienceLevelSystem.cs
+++ Assets/Script/ExperienceLevelSystem.cs
@@ -12,16 +12,25 @@ public class ExperienceLevelSystem : MonoBehaviour
     }
 
     public int currentExperience;
+    public int currentLevel;
+    public int levelCount;
+    public List<int> level;
     // Start is called before the first frame update
     void Start()
     {
-        
+        while (level.Count < levelCount)
+        {
+            level.Add(Mathf.CeilToInt(level[level.Count - 1] * 1.1f));
+        }        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        while (currentExperience >= level[currentLevel])
+        {
+            LevelUp();
+        }    
     }
 
     public void GetExp(int experienceToGet)
@@ -32,5 +41,16 @@ public class ExperienceLevelSystem : MonoBehaviour
     public void SpawnExp (Vector3 Location, int expAmount)
     {
         Instantiate(pickUpExp, Location, Quaternion.identity).expValue = expAmount;
+    }
+    private void LevelUp()
+    {
+        currentExperience -= level[currentLevel];
+
+        currentLevel++;
+
+        if (currentLevel > levelCount)
+        {
+            currentLevel--;
+        }
     }
 }
