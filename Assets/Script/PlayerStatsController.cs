4021ace2dd54330d70e3ccc917a79d67fcc5c698
wangchengze <13056790885@163.com>
2024-07-29 19:17:58 +0800

    establish player stats

---
+++ Assets/Script/PlayerStatsController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatsController : MonoBehaviour
+{
+    public static PlayerStatsController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public List<PlayerStatValue> moveSpeed, health, pickupRange, maxWeapon;
+    public int moveSpeedLevelCount, healthLevelCount, pickupRangeLevelCount;
+    private int moveSpeedLevel, healthLevel, pickupRangeLevel, maxWeaponLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = moveSpeed.Count - 1; i < moveSpeedLevelCount; i++)
+        {
+            moveSpeed.Add(new PlayerStatValue(moveSpeed[i].cost + moveSpeed[1].cost, moveSpeed[i].value + (moveSpeed[1].value - moveSpeed[0].value)));
+        }
+        for (int i = health.Count - 1; i < healthLevelCount; i++)
+        {
+            health.Add(new PlayerStatValue(health[i].cost + health[1].cost, health[i].value + (health[1].value - health[0].value)));
+        }
+        for (int i = pickupRange.Count - 1; i < pickupRangeLevelCount; i++)
+        {
+            pickupRange.Add(new PlayerStatValue(pickupRange[i].cost + pickupRange[1].cost, pickupRange[i].value + (pickupRange[1].value - pickupRange[0].value)));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+[System.Serializable]
+public class PlayerStatValue
+{
+    public int cost;
+    public float value;
+    public PlayerStatValue(int newcost, float newValue)
+    {
+        cost = newcost;
+        value = newValue;
+    }
+}
